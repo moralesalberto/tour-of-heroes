a4881d83d8d169ce088312a3f8e11c055df09657
moralesalberto <alberto@moralitos.com>
2022-08-05 09:58:07 -0400

    up to the listing and details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from '../mock-heroes';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -16,6 +19,14 @@ export class HeroesComponent implements OnInit {
 	hero: Hero = {
 		id: 1,
 		name: 'Windstorm'
+	}
+	
+	heroes = HEROES;
+	
+	selectedHero?: Hero;
+	
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
 	}
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 12, name: "Dr. Nice"},
+	{ id: 13, name: "Bombasto"},
+	{ id: 14, name: "Magma"}
+];
